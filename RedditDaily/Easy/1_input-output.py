5f792035975ff8bf08db2f5c3f313589fe24ef75
santarini <32736507+santarini@users.noreply.github.com>
2017-12-11 20:16:11 -1000

    Add files via upload

---
+++ RedditDaily/Easy/1_input-output.py
@@ -0,0 +1,4 @@
+name = input("What is your name? \n")
+age = int(input("How old are you? \n"))
+location = input("Where do you live? \n")
+print("Your name is " + name + ", you are " + str(age) + " years old, you live in " + location)
