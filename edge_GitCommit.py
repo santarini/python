643ab202abf47f3291ed36f57915307a030cca9e
Makoa <32736507+santarini@users.noreply.github.com>
2017-12-18 13:57:56 -1000

    Update edge_GitCommit.py

--- edge_GitCommit.py
+++ edge_GitCommit.py
@@ -1,9 +1,10 @@
-import random
 import time
+import datetime
+import random
 from selenium import webdriver
 
 driver = webdriver.Edge('C:\Program Files\Python\MicrosoftWebDriver.exe')
-driver.get('https://github.com/santarini/seleniumPushes/blob/master/pushlog.md');
+driver.get('https://github.com/santarini/seleniumPushes/blob/master/pythonlog.md');
 time.sleep(3)
 page_body = driver.find_element_by_tag_name('body')
 page_body.send_keys('e')
@@ -11,9 +12,11 @@ time.sleep(3)
 js_text_box = driver.find_element_by_class_name('commit-create')
 js_text_box.click()
 content = driver.find_element_by_class_name('CodeMirror-code')
-content.send_keys('Testing 4 5 6 \n')
+timenow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+content.send_keys(timenow + ' >> Testing 4 5 6; \n')
 time.sleep(3)
 submit = driver.find_element_by_id('submit-file')
 time.sleep(3)
 submit.submit()
 driver.quit()
+
