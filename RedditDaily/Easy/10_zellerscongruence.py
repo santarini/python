69f58d555d19d5628cf832e8660fd3295ae14248
Makoa <32736507+santarini@users.noreply.github.com>
2017-12-28 01:31:51 -1000

    Update 10_zellerscongruence.py

--- RedditDaily/Easy/10_zellerscongruence.py
+++ RedditDaily/Easy/10_zellerscongruence.py
@@ -1,12 +1,17 @@
 #uses Zeller's congruence
-day = int(input("Day \n"))
+#http://www.careeranna.com/articles/find-day-for-given-date-quickly/
 month = int(input("Month \n"))
+day = int(input("Day \n"))
 year = int(input("Year \n"))
-y1 = int(str(year)[:2])
-y2 = int(str(year)[2:])
-monthList = [11,12,1,2,3,4,5,6,7,8,9,10]
+if month == 1 or month == 2:
+    year = year -1
+y1 = year%100 #int(str(year)[:2])
+y2 = year/100 #int(str(year)[2:])
+print(y1, y2)
+monthList = [13,14,3,4,5,6,7,8,9,10,11,12]
 m = monthList[month-1]
-f = (day + ((13 * (m -1))/5) + int(y2) +(int(y2)/4)+(int(y1)/4)-(2 * int(y1)))%7
-dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-w = dayList[int(f)]
+f = (day + int(13*(m+1)/5) + int(y1) +(int(y1)/4)+(int(y2)/4)-(2 * int(y2)))%7
+dayList = ["Saturday", "Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+print(f)
+w = dayList[int(f) -1]
 print(w)
