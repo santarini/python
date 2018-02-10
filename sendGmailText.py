32d5b0d4ce105a5f6057186f789ea1a4c9888997
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-09 23:15:21 -1000

    Update and rename sendEmailText.py to sendGmailText.py

---
+++ sendGmailText.py
@@ -0,0 +1,36 @@
+import time
+import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+#creates a browsers
+#driver = webdriver.Chrome('C:\Program Files\Python\Python36\chromedriver.exe')
+driver = webdriver.Edge('C:\Program Files\Python\Python36\MicrosoftWebDriver.exe')
+#navigates to a gmail
+driver.get('https://mail.google.com/mail/u/3/');
+#defines the body element of the page
+page_body = driver.find_element_by_tag_name('body')
+#creates new message
+time.sleep(5)
+page_body.send_keys('c')
+#define the "recipient" field
+time.sleep(1)
+recipient = driver.find_element_by_name('to')
+recipient.send_keys('reciving email')
+#define the "subject line" field
+subject = driver.find_element_by_name('subjectbox')
+timenow = datetime.datetime.now().strftime("%I:%M:%S %p")
+datenow = datetime.datetime.now().strftime("%Y-%m-%d")
+subject.send_keys("Application completed" + Keys.TAB + timenow)
+subject.send_keys(Keys.COMMAND + Keys.ENTER)
+#define the "body" field
+#emailBody = driver.find_element_by_id(":90")
+#emailBody.send_keys(timenow)
+#define the send button and click it
+#sendButton = driver.find_element_by_id(":7n")
+#sendButton.click()
+#close the programe
+time.sleep(5)
+driver.quit()
