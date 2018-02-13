d1924169d2be3f1c0630497b5d1b9d5078d1d3b7
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-13 09:57:10 -1000

    Create inputToArrray.py

---
+++ inputToArrray.py
@@ -0,0 +1,17 @@
+##########Number to array
+
+#recieve input
+startNumber = input("Please enter your number: ")
+#create array
+numberArray = [int(i) for i in startNumber]
+print(numberArray)
+
+
+
+##########String to array
+
+#recieve input
+startNumber = input("Please enter your string: ")
+#create array
+numberArray = [str(i) for i in startNumber]
+print(numberArray)
