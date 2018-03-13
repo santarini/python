a104c482ffd3c7385de75279052e644f17879582
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-12 14:38:57 -1000

    Update time.py

--- time.py
+++ time.py
@@ -1,4 +1,11 @@
 import datetime
 
 timenow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+current_month = datetime.datetime.now().month
+current_year = datetime.datetime.now().year
+current_day = datetime.datetime.now().day
+
 print(timenow)
+print(current_month)
+print(current_year)
+print(current_day)
