bc19d94fda820a9339b55da5d8924d195bd0319e
Makoa <32736507+santarini@users.noreply.github.com>
2018-04-18 10:14:23 -1000

    Update InputToList.py

--- InputToList.py
+++ InputToList.py
@@ -24,11 +24,3 @@ startStr = input("Please enter your string: ")
 strArray = [str(i) for i in startStr.split()]
 print(strArray)
 
-##########String to list
-myPets = ['Zophie', 'Pooka', 'Fat-tail']
-print('Enter a pet name:')
-name = input()
-if name not in myPets:
-    print('I do not have a pet named ' + name)
-else:
-    print(name + ' is my pet.')
