0f90cbf410760678f23b75026eb853a8b83476da
Makoa <32736507+santarini@users.noreply.github.com>
2018-04-17 16:10:14 -1000

    Update 10_hotlineBling.py

--- RedditDaily/Easy/10_hotlineBling.py
+++ RedditDaily/Easy/10_hotlineBling.py
@@ -12,3 +12,20 @@ def testForPhone(test):
 
 test = 'Call me at 415-555-1011 tomorrow. (415) 555-9999 is my office. 415.555.1999 is my land line.'
 testForPhone(test)
+
+
+import re
+def testForPhone2(test):
+    phoneNumRegex = re.compile(r'''(
+        (\d{3}|\(\d{3}\))?
+        (\s|\.|\-)?
+        \d{3}
+        (\s|\.|\-)
+        \d{4}
+        )''', re.VERBOSE)
+    mo = phoneNumRegex.findall(test)
+    print(mo)
+    
+
+test = 'Call me at 415-555-1011 tomorrow. (415) 555-9999 is my office. 415.555.1999 is my land line.'
+testForPhone2(test)
