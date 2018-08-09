01699de7d6fbc6243b9a9e0afd8f43a453389519
Makoa <32736507+santarini@users.noreply.github.com>
2018-08-08 15:29:34 -1000

    Update datetime.py

--- datetime.py
+++ datetime.py
@@ -3,5 +3,6 @@ import datetime
 
 dayofWeek = datetime.datetime.today().weekday()
 fullDate = datetime.datetime.now().strftime("%Y-%m-%d")
+timeNow = datetime.datetime.now().strftime("%H:%M:%S")
 weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday", "Sunday"]
-print(weekdays[dayofWeek] + " " + fullDate)
+print(weekdays[dayofWeek] + " " + fullDate + " " + timeNow)
