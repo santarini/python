dd5d089f7ade7d765bba93bb19e7bb18122073f3
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-11 12:56:39 -1000

    Update 27_centuryLeap.py

--- RedditDaily/Easy/27_centuryLeap.py
+++ RedditDaily/Easy/27_centuryLeap.py
@@ -1,32 +1,38 @@
 import math
+import datetime
 
 def centuryCal():
     yearInput = input("Enter year: \n")
 
     century = int(yearInput)/100
 
+    now = datetime.datetime.now()
+
 #check century
-    if math.ceil(century) > 3:
-        print(str(yearInput)+ " was in the year " + str(math.ceil(century)) + "th Century")
+    if int(yearInput) > int(now.year):
+        print(str(yearInput)+ " will be in the " + str(math.ceil(century)) + "th Century")
+    elif int(yearInput) > int(now.year):
+        print(str(yearInput)+ " is in the " + str(math.ceil(century)) + "th Century")
+    elif math.ceil(century) > 3:
+        print(str(yearInput)+ " was in the " + str(math.ceil(century)) + "th Century")
     elif math.ceil(century) == 3:
-        print(str(yearInput)+ " was in the year " + str(math.ceil(century)) + "rd Century")
+        print(str(yearInput)+ " was in the " + str(math.ceil(century)) + "rd Century")
     elif math.ceil(century) == 2:
-        print(str(yearInput)+ " was in the year " + str(math.ceil(century)) + "nd Century")
+        print(str(yearInput)+ " was in the " + str(math.ceil(century)) + "nd Century")
     elif math.ceil(century) == 1:
-        print(str(yearInput)+ " was in the year " + str(math.ceil(century)) + "st Century")
+        print(str(yearInput)+ " was in the " + str(math.ceil(century)) + "st Century")
 
 #check if leap year
     if not(int(yearInput) % 4 == 0):
-        print(str(yearInput)+ " was a Common Year")
+        print(str(yearInput)+ " is a Common Year")
     elif not(int(yearInput) % 100 == 0):
-        print(str(yearInput)+ " was a Leap Year")
+        print(str(yearInput)+ " is a Leap Year")
     elif not(int(yearInput) % 400 == 0):
-        print(str(yearInput)+ " was a Common Year")
+        print(str(yearInput)+ " is a Common Year")
     else:
-        print(str(yearInput)+ " was a Common Year")
+        print(str(yearInput)+ " is a Common Year")
 
     print("\n")
     
     centuryCal()
 centuryCal()
-
