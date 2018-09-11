bd4a5eeceeb0f9c60359e88ef12836441475beab
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-11 13:22:38 -1000

    Update 27_centuryLeap.py

--- RedditDaily/Easy/27_centuryLeap.py
+++ RedditDaily/Easy/27_centuryLeap.py
@@ -1,38 +1,41 @@
-import datetime
 import math
+import datetime
 
 def centuryCal():
     yearInput = input("Enter year: \n")
-
-    century = int(yearInput)/100
-
+    century = math.ceil(int(yearInput)/100)
     now = datetime.datetime.now()
 
-#check century
+#tense modifier
+    if int(yearInput) == int(now.year):
+        tense = " is "
+    if int(yearInput) < int(now.year):
+        tense = " was "
     if int(yearInput) > int(now.year):
-        print(str(yearInput)+ " will be in the " + str(math.ceil(century)) + "th Century")
-    elif int(yearInput) > int(now.year):
-        print(str(yearInput)+ " is in the " + str(math.ceil(century)) + "th Century")
-    elif math.ceil(century) > 3:
-        print(str(yearInput)+ " was in the " + str(math.ceil(century)) + "th Century")
-    elif math.ceil(century) == 3:
-        print(str(yearInput)+ " was in the " + str(math.ceil(century)) + "rd Century")
-    elif math.ceil(century) == 2:
-        print(str(yearInput)+ " was in the " + str(math.ceil(century)) + "nd Century")
-    elif math.ceil(century) == 1:
-        print(str(yearInput)+ " was in the " + str(math.ceil(century)) + "st Century")
+        tense = " will be "
+
+#determine century
+    if str(century).endswith("3") and not(str(century).startswith("1")):
+        print(yearInput + tense + "in the " + str(century) + "rd Century")
+    if str(century).endswith("2") and not(str(century).startswith("1")):
+        print(yearInput + tense + "in the " + str(century) + "nd Century")
+    if str(century).endswith("1") and not(str(century).startswith("1")) or century == 1 :
+        print(yearInput + tense + "in the " + str(century) + "st Century")
+    if century > 3:
+        print(yearInput + tense + "in the " + str(century) + "th Century")
 
 #check if leap year
     if not(int(yearInput) % 4 == 0):
-        print(str(yearInput)+ " is a Common Year")
+        print(str(yearInput) + tense +  "a Common Year")
     elif not(int(yearInput) % 100 == 0):
-        print(str(yearInput)+ " is a Leap Year")
+        print(str(yearInput) + tense +  "a Leap Year")
     elif not(int(yearInput) % 400 == 0):
-        print(str(yearInput)+ " is a Common Year")
+        print(str(yearInput) + tense +  "a Common Year")
     else:
-        print(str(yearInput)+ " is a Common Year")
+        print(str(yearInput) + tense + "a Common Year")
 
+#give some breathing room
     print("\n")
-    
+
     centuryCal()
 centuryCal()
