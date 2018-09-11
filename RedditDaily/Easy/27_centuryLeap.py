c517c0f389bbeaa04d237083647d23bdc898e9cc
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-11 12:48:05 -1000

    Update 27_centuryLeap.py

--- RedditDaily/Easy/27_centuryLeap.py
+++ RedditDaily/Easy/27_centuryLeap.py
@@ -5,6 +5,7 @@ def centuryCal():
 
     century = int(yearInput)/100
 
+#check century
     if math.ceil(century) > 3:
         print(str(yearInput)+ " was in the year " + str(math.ceil(century)) + "th Century")
     elif math.ceil(century) == 3:
@@ -13,6 +14,19 @@ def centuryCal():
         print(str(yearInput)+ " was in the year " + str(math.ceil(century)) + "nd Century")
     elif math.ceil(century) == 1:
         print(str(yearInput)+ " was in the year " + str(math.ceil(century)) + "st Century")
+
+#check if leap year
+    if not(int(yearInput) % 4 == 0):
+        print("Common Year")
+    elif not(int(yearInput) % 100 == 0):
+        print("Leap Year")
+    elif not(int(yearInput) % 400 == 0):
+        print("Common Year")
+    else:
+        print("Common Year")
+
     print("\n")
+    
     centuryCal()
 centuryCal()
+
