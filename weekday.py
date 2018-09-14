3f84113dac428bb287e3a777abd6345775f20569
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-13 14:58:28 -1000

    Update weekday.py

--- weekday.py
+++ weekday.py
@@ -1,4 +1,11 @@
-import datetime
+from datetime import datetime, timedelta
 weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday", "Sunday"]
-today = datetime.datetime.today().weekday()
-print(weekdays[today])
+today = datetime.today().weekday()
+todayDate = datetime.today()
+beginWeek = datetime.today() - timedelta(days=(today+1))
+endWeek = beginWeek + timedelta(days=6)
+print("Today is " + weekdays[today])
+print(today)
+print("Today's date is " + todayDate.strftime("%Y-%m-%d"))
+print("Sunday's date is " + beginWeek.strftime("%Y-%m-%d"))
+print("Saturday's date is " + endWeek.strftime("%Y-%m-%d"))
