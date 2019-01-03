bd70d34e7b81b521922dc516d2da148a64716ce3
Makoa <32736507+santarini@users.noreply.github.com>
2019-01-03 10:35:12 -1000

    Update Easter.py

--- Easter.py
+++ Easter.py
@@ -1,1 +1,20 @@
+import datetime
 
+now = datetime.datetime.now()
+
+##Easter
+def Easter(year):
+    year = 2019
+    a = year % 19
+    b = year >> 2
+    c = b // 25 + 1
+    d = (c * 3) >> 2
+    e = ((a * 19) - ((c * 8 + 5) // 25) + d + 15) % 30
+    e += (29578 - a - e * 32) >> 10
+    e -= ((year % 7) + b - d + e + 2) % 7
+    d = e >> 5
+    day = e - d * 31
+    month = d + 3
+    return(datetime.date(year, month, day))
+  
+Easter(now.year)
